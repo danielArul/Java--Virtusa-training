b01752a5eac8ec9eed630c1dac669af1690b57e0
ChathurangaSN <sachithmaths@gmail.com>
2019-08-26 11:41:20 +0530

    Added min and max function in filteExercise

--- Homework 3/EX2/src/Filter.java
+++ Homework 3/EX2/src/Filter.java
@@ -1,3 +1,4 @@
+import java.util.Comparator;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
@@ -9,23 +10,44 @@ public class Filter {
     }
 
 
+    static void filterWithStream() {
+        List<Student> students = Student.getStudents().stream().filter(s -> s.getName().length() <= 6).collect(Collectors.toList());
+        System.out.println(students.toString());
+    }
 
 
+    public static void printStudentsWithShortnames() {
 
-        static void filterWithStream(){
-            List<Student> students=Student.getStudent().stream().filter(s->s.getName().length()<=6).collect(Collectors.toList());
-            System.out.println(students);
-        }
-
-        void printStudentsWithShortNames(){
-
-            List<Student> students = Student.getStudent();
-
-            for (Student student : students) {
-                if (student.getName().length() <= 5) {
-                    System.out.println(student.getName());
-                }
+        List<Student> students = Student.getStudents();
+        for (Student student : students) {
+            if (student.getName().length() <= 6) {
+                System.out.println(student.getName());
             }
         }
+    }
+
+    static void processWithoutStream() {
+        Student.getStudents().stream().map(s -> new Student("Dr" + s.getName(), s.getId())).collect(Collectors.toList()).forEach(s -> System.out.println(s.getName()));
+    }
+
+    static void processWithStream() {
+        List<Student> students = Student.getStudents().stream().map(s -> new Student("Dr" + s.getName(), s.getId())).collect(Collectors.toList());
+        System.out.println(students);
+    }
+
+    static void minimum(){
+        Student stu=Student.getStudents().stream().min(Comparator.comparing(Student::getId)).get();
+        System.out.println(stu.getName());
+    }
+
+    static void maximum(){
+        Student stu=Student.getStudents().stream().max(Comparator.comparing(Student::getId)).get();
+        System.out.println(stu.getName());
+    }
+
+    static void count(){
+        long count=Student.getStudents().stream().count();
+        System.out.println(count);
+    }
     }
 }
