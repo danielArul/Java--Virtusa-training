327d6948d6b2f4eb75e846d98cc96ea1c670d9cb
ChathurangaSN <sachithmaths@gmail.com>
2019-09-12 19:49:35 +0530

    authserver

--- auth-server (2)/src/main/java/com/example/rentcloud/auth/authserver/AuthServerApplication.java
+++ auth-server (2)/src/main/java/com/example/rentcloud/auth/authserver/AuthServerApplication.java
@@ -2,9 +2,10 @@ package com.example.rentcloud.auth.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
-@EnableAuthorizaionServer
+@EnableAuthorizationServer
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
